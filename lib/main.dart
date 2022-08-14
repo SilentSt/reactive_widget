f730e8b038867bcb90f4bc00559c0f4ed0eb7aa2
Maxim Pervushin <sekyret1@yandex.ru>
2022-08-14 19:23:08 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:reactive_widget/test/test_provider.dart';
+import 'package:reactive_widget/test/test_wm.dart';
 
 import 'test/test_widget.dart';
 
