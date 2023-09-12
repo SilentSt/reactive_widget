0a1da18128fcf2dadb1235ea56eb287d8fd440cc
Maxim Pervushin <sekyret1@yandex.ru>
2023-09-12 14:28:19 +0300

    fix

--- lib/reactive/reactive_builder.dart
+++ lib/reactive/reactive_builder.dart
@@ -14,13 +14,16 @@ class ReactiveBuilderWidget<T> extends StatefulWidget {
 
   ///основной билдер отвечающий за отрисовку UI
   final ReactiveBuilder<T> builder;
+
   ///контейнер который отвечает за хранение значения представляет собой своеобразную модель,
   ///этот виджет подписывается на этот контейнер и когда конейнер уведомляет об изменениях
   ///UI перерисовывается
   final ReactiveContainer<T?> container;
+
   ///опциональный параметр, базовое значение - `true`, позволяет отрисовывать специальный виджет "Загрузки",
   ///когда `container.isBusy==true` он будет отображаться вместов `builder`
   final bool displayLoadingWidget;
+
   ///опциональный параметр позволяющий изменить виджет который будет отрисовываться во время загрузки
   ///если не указан, то будет отображаться - `CupertinoActivityIndicator(color: CupertinoColors.activeBlue)`
   final Widget? loadingWidget;
@@ -32,7 +35,7 @@ class _ReactiveBuilderWidgetState<T> extends State<ReactiveBuilderWidget<T>> {
   @override
   void initState() {
     widget.container.addListener(() {
-      setState(() {});
+      if (mounted) setState(() {});
     });
     super.initState();
   }
